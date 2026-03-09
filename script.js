33615f2961f9509a1a4d358d75010c8eba5a8907
Yasir Awaan <yasirawan2847@gmail.com>
2026-03-09 08:02:31 +0500

    Add Songs & Improve Ui

---
+++ script.js
@@ -0,0 +1,69 @@
+console.log("shanti");
+
+// async function getSongs() {
+//   let a = await fetch("http://192.168.100.6:3000/Assets/songs/");
+//   let response = await a.text();
+//   console.log(response);
+//   let div = document.createElement("div")
+//   div.innerHTML=response
+//   let as = document.getElementsByTagName("a")
+//   songs =[]
+//   for (let index = 0; index < as.length; index++) {
+//     const element = as[index];
+//     if(element.href.endsWith(".mp3")){
+//       songs.push(element.href)
+//     }
+//   console.log(songs);
+
+// }
+// async function main() {
+//   let songs = await getSongs();
+//   console.log(songs);
+// }
+
+// main()
+// }
+
+async function getSongs() {
+  let a = await fetch("http://192.168.100.6:3000/Assets/songs/");
+  let response = await a.text();
+  console.log(response);
+
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href);
+    }
+  }
+  return songs;
+}
+
+async function main() {
+  // Get The List Of The All Songs
+  let songs = await getSongs();
+  console.log(songs);
+
+  let songUl = document.querySelector(".songlist").getElementsByTagName("ul")[0];
+
+  for (const song of songs) {
+    let songName = decodeURIComponent(song);
+    songName = songName.split("\\").pop(); 
+    songName = songName.replace(".mp3", ""); 
+
+    songUl.innerHTML += `<li>${songName}</li>`;
+  }
+  var audio = new Audio(songs[0]);
+  // audio.play();
+
+  audio.addEventListener("loadeddata", () => {
+    let duration = audio.duration;
+    console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    // The duration variable now holds the duration (in seconds) of the audio clip
+  });
+}
+
+main();
