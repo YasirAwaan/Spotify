7bcdc5f492c8c2ee0336202ed3b1caeb7ca40849
Yasir Awaan <yasirawan2847@gmail.com>
2026-03-11 07:33:40 +0500

    Add seekbar & Audio fix

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log("shanti");
 
+let songs = []; // global
+let currentSong = new Audio();
 async function getSongs() {
   let a = await fetch("http://192.168.100.6:3000/Assets/songs/");
   let response = await a.text();
@@ -18,9 +20,15 @@ async function getSongs() {
   return songs;
 }
 
+const playMusic = (track) => {
+  currentSong.pause();
+  currentSong.currentTime = 0;
+  currentSong.src = track;
+  currentSong.play();
+};
 async function main() {
   // Get The List Of The All Songs
-  let songs = await getSongs();
+  songs = await getSongs();
   console.log(songs);
 
   let songUl = document
@@ -28,28 +36,40 @@ async function main() {
     .getElementsByTagName("ul")[0];
 
   for (const song of songs) {
-    let songName = decodeURIComponent(song);
-    songName = songName.split("\\").pop();
-    songName = songName.replace(".mp3", "");
-
+    let songName = decodeURIComponent(song)
+      .split(/[/\\]/)
+      .pop()
+      .replace(".mp3", "");
     songUl.innerHTML += `
       <li>
         <img class="invert" src="Assets/img/music.svg" alt="">
         <div class="info">
           <div>${songName}</div>
-          <div>Song Artist</div>
+          <div></div>
         </div>
         <img class="invert" src="Assets/img/play.svg" alt="">
       </li>
     `;
   }
 
-  var audio = new Audio(songs[0]);
-  // audio.play();
+  // var audio = new Audio(songs[0]);
 
-  audio.addEventListener("loadeddata", () => {
-    let duration = audio.duration;
-    console.log(audio.duration, audio.currentSrc, audio.currentTime);
+  currentSong.addEventListener("loadeddata", () => {
+    let duration = currentSong.duration;
+    console.log(
+      currentSong.duration,
+      currentSong.currentSrc,
+      currentSong.currentTime,
+    );
+  });
+
+  // Attach an Event Listener To Each Song
+  Array.from(
+    document.querySelector(".songList").getElementsByTagName("li"),
+  ).forEach((e, index) => {
+    e.addEventListener("click", () => {
+      playMusic(songs[index]);
+    });
   });
 }
 
